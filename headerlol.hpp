8c2948eb14d53148be46970a7028519f7db29d04
kuzipam <munemoashley2006@gmail.com>
2025-09-12 17:33:32 -0500

    Update headerlol.hpp

--- headerlol.hpp
+++ headerlol.hpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
 
-void primefactorization(std::vector<int> factors, int num);
 void primefactorization(std::vector<int> factors, int num, int divisor);
 std::vector <int> primefactorization(int num);
